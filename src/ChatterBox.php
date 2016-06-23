a89612dab2f7179f6ba32600cfdb8d034ca68dae
Upitdc Prado <upitdc.prado@gmail.com>
2016-06-23 19:49:57 +0800

    + ChatterBoxSendPy.php, Back Up for script that writes directly to the python socket server - ChatterBox,     - Connection to Senslopedb Database and creation of db if it does not exist yet     - Creation of 'smsinbox' and 'smsoutbox' if it does not exist yet     - Segregation of message: sent and received     - Writing of parsed messages to database

--- src/ChatterBox.php
+++ src/ChatterBox.php
@@ -5,10 +5,124 @@ use Ratchet\ConnectionInterface;
 
 class ChatterBox implements MessageComponentInterface {
     protected $clients;
+    protected $dbconn;
 
     public function __construct() {
         $this->clients = new \SplObjectStorage;
+
+        //Create a DB Connection
+        $host = "localhost";
+        $usr = "root";
+        $pwd = "senslope";
+        $dbname = "senslopedb";
+
+        $this->dbconn = new \mysqli($host, $usr, $pwd);
+
+        if ($this->dbconn->connect_error) {
+            die("Connection failed: " . $this->dbconn->connect_error);
+        }
+        echo "Successfully connected to database!\n";
+
+        $this->connectSenslopeDB();
+        echo "Switched to schema: senslopedb!\n";
+
+        $this->createSMSInboxTable();
+        $this->createSMSOutboxTable();
     }
+
+    //Connect to senslopedb
+    public function connectSenslopeDB() {
+        //$success = $this->dbconn->mysqli_select_db("senslopedb");
+        $success = mysqli_select_db($this->dbconn, "senslopedb");
+
+        if (!$success) {
+            $this->createSenslopeDB();
+        }
+    }
+
+    //Create database if it does not exist yet
+    public function createSenslopeDB() {
+        $sql = "CREATE DATABASE senslopedb";
+        if ($this->dbconn->query($sql) === TRUE) {
+            echo "Database created successfully\n";
+        } else {
+            die("Error creating database: " . $this->dbconn->error);
+        }
+    }
+
+    //Create the smsinbox table if it does not exist yet
+    public function createSMSInboxTable() {
+        $sql = "CREATE TABLE IF NOT EXISTS `senslopedb`.`smsinbox` (
+                  `sms_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                  `timestamp` DATETIME NULL,
+                  `sim_num` VARCHAR(20) NULL,
+                  `sms_msg` VARCHAR(1023) NULL,
+                  `read_status` VARCHAR(20) NULL,
+                  `web_flag` VARCHAR(2) NOT NULL DEFAULT 'WU',
+                  PRIMARY KEY (`sms_id`))";
+
+        if ($this->dbconn->query($sql) === TRUE) {
+            echo "Table 'smsinbox' exists!\n";
+        } else {
+            die("Error creating table 'smsinbox': " . $this->dbconn->error);
+        }
+    }
+
+    //Create the smsoutbox table if it does not exist yet
+    public function createSMSOutboxTable() {
+        $sql = "CREATE TABLE IF NOT EXISTS `senslopedb`.`smsoutbox` (
+                  `sms_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                  `timestamp_written` DATETIME NULL,
+                  `timestamp_sent` DATETIME NULL,
+                  `recepients` VARCHAR(1023) NULL,
+                  `sms_msg` VARCHAR(1023) NULL,
+                  `send_status` VARCHAR(20) NOT NULL DEFAULT 'UNSENT',
+                  PRIMARY KEY (`sms_id`))";
+
+        if ($this->dbconn->query($sql) === TRUE) {
+            echo "Table 'smsoutbox' exists!\n";
+        } else {
+            die("Error creating table 'smsoutbox': " . $this->dbconn->error);
+        }
+    }
+
+    //Insert data for smsinbox table
+    public function insertSMSInboxEntry($smsid, $timestamp, $sender, $message) {
+        //TODO: this needs to filter or check special characters
+
+        $sql = "INSERT INTO smsinbox (sms_id, timestamp, sim_num, sms_msg, read_status, web_flag)
+                VALUES ('$smsid', '$timestamp', '$sender', '$message', 'READ-FAIL', 'WS')";
+
+        if ($this->dbconn->query($sql) === TRUE) {
+            echo "New record created successfully!\n";
+        } else {
+            echo "Error: " . $sql . "<br>" . $this->dbconn->error;
+        }
+    }
+
+    //Insert data for smsoutbox table
+    public function insertSMSOutboxEntry($recipients, $message) {
+        //TODO: this needs to filter or check special characters
+
+        foreach ($recipients as $recipient) {
+            $curTime = date("Y-m-d H:i:s", time());
+            echo "$curTime Message recipient: $recipient\n";
+
+            $sql = "INSERT INTO smsoutbox (timestamp_written, recepients, sms_msg, send_status)
+                    VALUES ('$curTime', '$recipient', '$message', 'PENDING')";
+
+            if ($this->dbconn->query($sql) === TRUE) {
+                echo "New record created successfully!\n";
+            } else {
+                echo "Error: " . $sql . "<br>" . $this->dbconn->error;
+            }
+        }
+    }
+
+    //TODO: Resilience against Net Connection Loss
+    //Create a protocol for checking whether the message was sent to GSM.
+    //There should be a function that will attempt to send "PENDING" data
+    //  to GSM everytime there is a new connection.
 
     public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
@@ -19,22 +133,57 @@ class ChatterBox implements MessageComponentInterface {
 
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n", $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-                
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
+
+        $decodedText = json_decode($msg);
+
+        if ($decodedText == NULL) {
+            echo "Message is not in JSON format ($msg).\n";
+            return;
+        }
+        else {
+            echo "Valid data\n";
+            echo sprintf('Connection %d sending message "%s" to %d other connection%s' . 
+                    "\n", $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+            $msgType = $decodedText->type;
+
+            if (($msgType == "smssend") || ($msgType == "smsrcv"))  {
+                //TODO: broadcast JSON message to all connected clients
+                foreach ($this->clients as $client) {
+                    if ($from !== $client) {
+                        // The sender is not the receiver, send to each client connected
+                        $client->send($msg);
+                    }
+                }
+
+                //TODO: save message in DB (maybe create a thread to handle the DB write for the sake of scalability)
+                //saving "smssend"
+                if ($msgType == "smssend") {
+                    echo "Message sent by ChatterBox Users to GSM and the community.\n";
+
+                    //store data in 'smsoutbox' table
+                    $recipients = $decodedText->numbers;
+                    $sentMsg = $decodedText->msg;
+
+                    $this->insertSMSOutboxEntry($recipients, $sentMsg);
+                }
+                //saving "smsrcv"
+                elseif ($msgType == "smsrcv") {
+                    echo "Message received from GSM.\n";
+
+                    //store data in 'smsinbox' table
+                    $smsid = $decodedText->sms_id;
+                    $rcvTS = $decodedText->timestamp;
+                    $sender = $decodedText->sender;
+                    $rcvMsg = $decodedText->msg;
+
+                    $this->insertSMSInboxEntry($smsid, $rcvTS, $sender, $rcvMsg);
+                }
+            } 
+            else {
+                echo "Message will be ignored\n";
             }
         }
-
-        $host = "127.0.0.1";
-        $port = 5051;
-
-        $pySocketServer = socket_create(AF_INET, SOCK_STREAM, 0) or die("Can't create socket\n");
-        $result = socket_connect($pySocketServer, $host, $port) or die("Can't connect to server\n");
-        socket_write($pySocketServer, $msg, strlen($msg)) or die("Can't send data to server\n");
-        socket_close($pySocketServer);
     }
 
     public function onClose(ConnectionInterface $conn) {
