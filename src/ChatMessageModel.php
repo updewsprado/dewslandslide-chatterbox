f7e2843a9863f2b07ba8e76943c22a2b19c883ab
John Geliberte <geliberteofficials@gmail.com>
2018-06-21 16:58:53 +0800

    minimize gintags data to be return

--- src/ChatMessageModel.php
+++ src/ChatMessageModel.php
@@ -3527,10 +3527,15 @@ class ChatMessageModel {
     }
 
     function fetchSmsTags($sms_id) {
+        $tags = [];
         $get_tags_query = "SELECT * FROM gintags INNER JOIN gintags_reference ON tag_id_fk = gintags_reference.tag_id WHERE table_element_id = '".$sms_id."';";
         $execute_query = $this->dbconn->query($get_tags_query);
         if ($execute_query->num_rows > 0) {
             $full_data['data'] = $execute_query->fetch_assoc();
+            while ($row = $execute_query->fetch_assoc()) {
+                array_push($tags,$row['tag_name']);
+            }
+            $full_data['data'] = $tags;
         } else {
             $full_data['data'] = [];
         }
