bc2af5fa79fc5da0c19ae4ac0437abde1c742888
John Geliberte <geliberteofficials@gmail.com>
2018-08-28 14:04:43 +0800

    Change IP to x.x.x.75

--- utils/config.php
+++ utils/config.php
@@ -2,7 +2,7 @@
 	return [
 	    "dbcredentials" => [
 		    'dbhost' => '192.168.150.75',
-		    'dbname' => 'senslopedb',
+		    'dbname' => 'comms_db',
 		    'dbuser' => 'dyna_staff',
 		    'dbpass' => 'accelerometer'
 		    ],
