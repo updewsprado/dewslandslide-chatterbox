9f9061fcee391dec7a4e4eb96c65903253db2ddb
John Geliberte <geliberteofficials@gmail.com>
2018-09-01 17:08:00 +0800

    Added EWI dashboard

--- utils/config.php
+++ utils/config.php
@@ -1,10 +1,23 @@
 <?php
+	// return [
+	//     "dbcredentials" => [
+	// 	    'dbhost' => '192.168.150.75',
+	// 	    'dbname' => 'comms_db',
+	// 	    'dbuser' => 'pysys_local',
+	// 	    'dbpass' => 'NaCAhztBgYZ3HwTkvHwwGVtJn5sVMFgg'
+	// 	    ],
+	// 	"wsscredentials" => [
+	// 		'host' => 'localhost',
+	// 		'port' => '5050'
+	// 	]
+	// ];
+
 	return [
 	    "dbcredentials" => [
-		    'dbhost' => '192.168.150.75',
+		    'dbhost' => 'localhost',
 		    'dbname' => 'comms_db',
-		    'dbuser' => 'pysys_local',
-		    'dbpass' => 'NaCAhztBgYZ3HwTkvHwwGVtJn5sVMFgg'
+		    'dbuser' => 'root',
+		    'dbpass' => 'senslope'
 		    ],
 		"wsscredentials" => [
 			'host' => 'localhost',
