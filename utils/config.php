b281e197943d8490a3b2d889ee13dfc44619851c
jhndvdgvr <david063095@gmail.com>
2018-09-20 13:58:42 +0800

    initials fix for A0 lowering and extended ewi template

--- utils/config.php
+++ utils/config.php
@@ -1,27 +1,27 @@
 <?php
-	return [
-	    "dbcredentials" => [
-		    'dbhost' => '192.168.150.75',
-		    'dbname' => 'comms_db',
-		    'dbuser' => 'pysys_local',
-		    'dbpass' => 'NaCAhztBgYZ3HwTkvHwwGVtJn5sVMFgg'
-		    ],
-		"wsscredentials" => [
-			'host' => 'localhost',
-			'port' => '5050'
-		]
-	];
-
 	// return [
 	//     "dbcredentials" => [
-	// 	    'dbhost' => 'localhost',
+	// 	    'dbhost' => '192.168.150.75',
 	// 	    'dbname' => 'comms_db',
-	// 	    'dbuser' => 'root',
-	// 	    'dbpass' => 'senslope'
+	// 	    'dbuser' => 'pysys_local',
+	// 	    'dbpass' => 'NaCAhztBgYZ3HwTkvHwwGVtJn5sVMFgg'
 	// 	    ],
 	// 	"wsscredentials" => [
 	// 		'host' => 'localhost',
 	// 		'port' => '5050'
 	// 	]
 	// ];
+
+	return [
+	    "dbcredentials" => [
+		    'dbhost' => 'localhost',
+		    'dbname' => 'comms_db',
+		    'dbuser' => 'root',
+		    'dbpass' => 'senslope'
+		    ],
+		"wsscredentials" => [
+			'host' => 'localhost',
+			'port' => '5050'
+		]
+	];
 // ?>
